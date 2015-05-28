600117a906702ad24ea878812491fa6df98d1c65
Viswanth Chadalawada <viswanth.chadalawada@gmail.com>
2015-05-27 23:05:27 -0700

    Initial Commit

---
+++ Out of this World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of this World
+//
+//  Created by Srikanth on 5/27/15.
+//  Copyright (c) 2015 ViswanthWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Out of this World/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Out of this World
+//
+//  Created by Srikanth on 5/27/15.
+//  Copyright (c) 2015 ViswanthWorks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
